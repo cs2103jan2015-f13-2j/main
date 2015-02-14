9a13516bdc9b8bd3343d8ea7f9664ca3f86444c7
taipan541 <taipan541@hotmail.com>
2015-02-14 12:57:29 +0800

    Include GSON library, for serializing/deserializing objects. Include sample usage in FileIO.java.

--- WorkOnIt/src/data/FileIO.java
+++ WorkOnIt/src/data/FileIO.java
@@ -1,6 +1,9 @@
 package data;
 
 import java.util.ArrayList;
+
+import com.google.gson.Gson;
+
 import entity.Task;
 
 public class FileIO {
@@ -23,4 +26,35 @@ public class FileIO {
 		return taskList;
 	}
 
+	/*
+	 * Sample Usage of GSON:
+	 * 
+	 *  Serializing :
+	 * 	Error e1 = new Error(true, "first message..");
+	 *	String json = serializeToJson(e1);
+	 *	System.out.println(json);
+	 *	
+	 *  Deserializing :
+	 *	Error e2 = (Error) deserializeFromJson(json, e1.getClass());
+	 *	System.out.println(e2.toString());
+	 */
+	
+	
+	private static String serializeToJson(Object object) {
+		
+		String json;
+		
+		Gson gson = new Gson();
+		json = gson.toJson(object);
+		
+		return json;
+	}
+	
+	private static <T> Object deserializeFromJson(String json, Class<T> type) {
+		
+		Gson gson = new Gson();
+		Object object = gson.fromJson(json, type);
+		
+		return object;
+	}
 }
