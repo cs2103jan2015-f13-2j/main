787a6b19b6a99e42badba40a9767e11ca861aa5f
taipan541 <taipan541@hotmail.com>
2015-02-19 21:42:27 +0800

    updated application.Main.java to the appropriate size and running in borderless window.
    Reorganized css and graphic files to their respective package folder.

--- WorkOnIt/src/application/Main.java
+++ WorkOnIt/src/application/Main.java
@@ -2,6 +2,7 @@ package application;
 	
 import javafx.application.Application;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 
@@ -11,10 +12,15 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			
+			Scene scene = new Scene(root,600,100);
+			scene.getStylesheets().add(Main.class.getResource("../css/application.css").toExternalForm());
+			scene.setFill(null);
+	        
+	        primaryStage.initStyle(StageStyle.TRANSPARENT);		
 			primaryStage.setScene(scene);
 			primaryStage.show();
+			
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
