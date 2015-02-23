7368ad70b4715c1d4ea8521c436c302ff24a73f9
taipan541 <taipan541@hotmail.com>
2015-02-24 06:31:33 +0800

    Added Comparator entity.Task class and hashcode & equals method
    reference to issue #23.
    
    -include sortDate:Date field in entity.Task class.
    -default sortDate value is epoch time. different Task have different
    sortDate value. the following are:
    NormalTask > startDateTime
    DeadlineTask > deadline
    RecurrenceTask > startRecurrenceDate
    
    -All Task object can be sorted by calling entity.Task.taskComparator in
    order of:
    1) Sort Date in ascending order
    2) if date is equal, sort priority descending order (High > Medium >
    Low)
    3) if priority is equal, sort by task name alphabetically
    
    -include hashcode & equals method in all Task classes

--- WorkOnIt/src/entity/RecurrenceTask.java
+++ WorkOnIt/src/entity/RecurrenceTask.java
@@ -6,16 +6,17 @@ public class RecurrenceTask extends Task {
 
 	private long tagId;
 	private Date startRecurrenceDate, endRecurrenceDate;
-	
-	public RecurrenceTask(String taskName, int priority, Date startRecurrenceDate, Date endRecurrenceDate) {
-		
+
+	public RecurrenceTask(String taskName, int priority,
+			Date startRecurrenceDate, Date endRecurrenceDate) {
+
 		super(taskName, priority);
-		
+
 		long generatedTagId = System.currentTimeMillis();
 		this.setTagId(generatedTagId);
 		this.setStartRecurrenceDate(startRecurrenceDate);
 		this.setEndRecurrenceDate(endRecurrenceDate);
-		
+
 	}
 
 	public long getTagId() {
@@ -32,6 +33,7 @@ public class RecurrenceTask extends Task {
 
 	public void setStartRecurrenceDate(Date startRecurrenceDate) {
 		this.startRecurrenceDate = startRecurrenceDate;
+		super.setSortDate(startRecurrenceDate);
 	}
 
 	public Date getEndRecurrenceDate() {
@@ -40,6 +42,46 @@ public class RecurrenceTask extends Task {
 
 	public void setEndRecurrenceDate(Date endRecurrenceDate) {
 		this.endRecurrenceDate = endRecurrenceDate;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime
+				* result
+				+ ((endRecurrenceDate == null) ? 0 : endRecurrenceDate
+						.hashCode());
+		result = prime
+				* result
+				+ ((startRecurrenceDate == null) ? 0 : startRecurrenceDate
+						.hashCode());
+		result = prime * result + (int) (tagId ^ (tagId >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		RecurrenceTask other = (RecurrenceTask) obj;
+		if (endRecurrenceDate == null) {
+			if (other.endRecurrenceDate != null)
+				return false;
+		} else if (!endRecurrenceDate.equals(other.endRecurrenceDate))
+			return false;
+		if (startRecurrenceDate == null) {
+			if (other.startRecurrenceDate != null)
+				return false;
+		} else if (!startRecurrenceDate.equals(other.startRecurrenceDate))
+			return false;
+		if (tagId != other.tagId)
+			return false;
+		return true;
 	}
 
 	@Override
--- WorkOnIt/src/entity/Task.java
+++ WorkOnIt/src/entity/Task.java
@@ -1,5 +1,6 @@
 package entity;
 
+import java.util.Comparator;
 import java.util.Date;
 
 public abstract class Task {
@@ -7,14 +8,18 @@ public abstract class Task {
 	private String taskName;
 	private long taskId;
 	private int priority;
-	
+	private Date sortDate;
+
 	public Task(String taskName, int priority) {
-		
+
 		long generatedTaskId = System.currentTimeMillis();
-		
+
 		this.setTaskId(generatedTaskId);
 		this.setTaskName(taskName);
 		this.setPriority(priority);
+
+		long epochOffset = 0L;
+		this.setSortDate(new Date(epochOffset));
 	}
 
 	public long getTaskId() {
@@ -40,15 +45,97 @@ public abstract class Task {
 	public void setPriority(int priority) {
 		this.priority = priority;
 	}
-	
+
+	public Date getSortDate() {
+		return sortDate;
+	}
+
+	public void setSortDate(Date sortDate) {
+		this.sortDate = sortDate;
+	}
+
 	public Date getDateCreated() {
-		
+
 		Date dateCreated = null;
-		
+
 		dateCreated = new Date(this.taskId);
-		
+
 		return dateCreated;
 	}
-	
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + priority;
+		result = prime * result
+				+ ((sortDate == null) ? 0 : sortDate.hashCode());
+		result = prime * result + (int) (taskId ^ (taskId >>> 32));
+		result = prime * result
+				+ ((taskName == null) ? 0 : taskName.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Task other = (Task) obj;
+		if (priority != other.priority)
+			return false;
+		if (sortDate == null) {
+			if (other.sortDate != null)
+				return false;
+		} else if (!sortDate.equals(other.sortDate))
+			return false;
+		if (taskId != other.taskId)
+			return false;
+		if (taskName == null) {
+			if (other.taskName != null)
+				return false;
+		} else if (!taskName.equals(other.taskName))
+			return false;
+		return true;
+	}
+
+	public static Comparator<Task> taskComparator = new Comparator<Task>() {
+
+		@Override
+		public int compare(Task task1, Task task2) {
+
+			int returnVal;
+
+			// sort task date in ascending order
+			Date task1Date = task1.getSortDate();
+			Date task2Date = task2.getSortDate();
+
+			if (task1Date.equals(task2Date)) {
+				// sort priority in descending order
+				// (high > medium > low)
+				int task1Priority = task1.getPriority();
+				int task2Priority = task2.getPriority();
+
+				if (task1Priority == task2Priority) {
+					// sort task by name
+					String task1Desc = task1.getTaskName();
+					String task2Desc = task2.getTaskName();
+
+					returnVal = task1Desc.compareTo(task2Desc);
+				} else {
+					returnVal = task2Priority - task1Priority;
+				}
+
+			} else {
+				returnVal = task1Date.compareTo(task2Date);
+			}
+
+			return returnVal;
+		}
+	};
+
 	public abstract String toString();
 }
