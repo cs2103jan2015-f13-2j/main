4a3a0549cb662dca01b1de99ae7a930beabadfd4
taipan541 <taipan541@hotmail.com>
2015-02-25 22:03:26 +0800

    Included an Object field in entity.Success - almost all method will return Success object, which then will contain the object that they want to return, packaged in entitiy.Success object.
    close #27

--- WorkOnIt/src/entity/Success.java
+++ WorkOnIt/src/entity/Success.java
@@ -2,11 +2,18 @@ package entity;
 
 public class Success {
 	
+	private Object obj;
 	private boolean isSuccess;
 	private String message;
 
 	public Success(boolean isSuccess, String message) {
+
+		this(null, isSuccess, message);
+	}
+	
+	public Success(Object obj, boolean isSuccess, String message) {
 		
+		this.setObj(obj);
 		this.setSuccess(isSuccess);
 		this.setMessage(message);
 	}
@@ -27,9 +34,17 @@ public class Success {
 		this.message = message;
 	}
 
-	@Override
-	public String toString() {
-		return "Error [isSuccess=" + isSuccess + ", message=" + message + "]";
+	public Object getObj() {
+		return obj;
 	}
 
+	public void setObj(Object obj) {
+		this.obj = obj;
+	}
+
+	@Override
+	public String toString() {
+		return "Success [obj=" + obj + ", isSuccess=" + isSuccess
+				+ ", message=" + message + "]";
+	}
 }
