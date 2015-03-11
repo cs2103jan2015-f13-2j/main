2790e99405e2b1d6efb6ec4472fdf4a6e359c027
taipan541 <taipan541@hotmail.com>
2015-03-11 21:06:46 +0800

    amended parseCommand() in logic.Validator to return Success obj - parseCommand return Success obj - application.Main handle Success obj

--- WorkOnIt/src/application/Main.java
+++ WorkOnIt/src/application/Main.java
@@ -23,6 +23,7 @@ public class Main extends Application implements Runnable {
 	static List<String> elementList = null;
 	static List<String> secondaryList = null;
 	static Validator commandValidator = null;
+
 	// KeyListener listener = new KeyListener();
 
 	@Override
@@ -31,7 +32,7 @@ public class Main extends Application implements Runnable {
 			// listener.registerHook();
 			// GlobalScreen.addNativeKeyListener(listener);
 			Pane root = new Pane();
-//			primaryStage.initStyle(StageStyle.TRANSPARENT);
+			// primaryStage.initStyle(StageStyle.TRANSPARENT);
 			Scene scene = new Scene(root, 300, 100);
 			scene.getStylesheets().add(
 					Main.class.getResource("../css/application.css")
@@ -39,7 +40,7 @@ public class Main extends Application implements Runnable {
 			scene.setFill(null);
 			final TextField txtF = new TextField();
 			initializeGlobals();
-			
+
 			txtF.setId("textField");
 			txtF.setLayoutX(0);
 			txtF.setLayoutY(0);
@@ -77,36 +78,30 @@ public class Main extends Application implements Runnable {
 	public void run() {
 		launch();
 	}
-	
-	public void initializeGlobals(){
+
+	public void initializeGlobals() {
 		elementList = new ArrayList<String>();
 		secondaryList = new ArrayList<String>();
 		commandValidator = new Validator();
 	}
 
-	
 	protected void executeCommand(String commandString) {
-		Success successCheck = null;
-		
-		Object temp = commandValidator.parseCommand(commandString);
 
-		if (temp instanceof Success) {
-			successCheck = (Success) temp;
-			if (successCheck.isSuccess() == false) {
-				System.out.println(successCheck.getMessage());
-			} else {
-				System.out.println("Command executed successfully");
-			}
+		Success successCheck = commandValidator.parseCommand(commandString);
+
+		if (successCheck.isSuccess() == false) {
+			System.out.println(successCheck.getMessage());
 		} else {
-			System.out.println("executeCommand: Object not instance of success");
+			System.out.println("Command executed successfully");
 		}
 
 		// System.out.println(obj.getTaskName()
 
 	}
 
-	public static void commandHandler(KeyEvent event, String textFieldText, final Stage stage) {
-		if(event.getCode().getName().equals("Esc")){
+	public static void commandHandler(KeyEvent event, String textFieldText,
+			final Stage stage) {
+		if (event.getCode().getName().equals("Esc")) {
 			hide(stage);
 		}
 		// detects a space, handle new word
@@ -118,11 +113,10 @@ public class Main extends Application implements Runnable {
 
 	private static void wordHandler(String textFieldText) {
 
-		
 		String[] stringArr = textFieldText.trim().split(" ");
-		
+
 		elementList.clear();
-		
+
 		// iterate thru the input
 		for (int i = 0; i < stringArr.length; i++) {
 			elementList.add(stringArr[i]);
@@ -142,22 +136,22 @@ public class Main extends Application implements Runnable {
 	private static void handleMethod(String input) {
 		System.out.println(input);
 	}
-	
+
 	private static void hide(final Stage stage) {
-        Platform.runLater(new Runnable() {
-            @Override
-            public void run() {
-                if (SystemTray.isSupported()) {
-                    stage.hide();
-                } else {
-                    System.exit(0);
-                }
-            }
-        });
-    }
-	
+		Platform.runLater(new Runnable() {
+			@Override
+			public void run() {
+				if (SystemTray.isSupported()) {
+					stage.hide();
+				} else {
+					System.exit(0);
+				}
+			}
+		});
+	}
+
 	public static void main(String[] args) {
-		
+
 		launch();
 	}
 }
