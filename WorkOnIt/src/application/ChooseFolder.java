922f640b36586fc4a84260abc0a6c4f6845d3c5f
taipan541 <taipan541@hotmail.com>
2015-03-24 14:54:54 +0800

    include pop up box before choosing save directory -include controlsfx library -include javafx dialogs official library

--- WorkOnIt/src/application/ChooseFolder.java
+++ WorkOnIt/src/application/ChooseFolder.java
@@ -9,14 +9,37 @@ import resource.Message;
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.embed.swing.JFXPanel;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Dialogs;
+import javafx.scene.control.Dialogs.DialogResponse;
+import javafx.scene.layout.VBoxBuilder;
+import javafx.scene.text.Text;
 import javafx.stage.DirectoryChooser;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 public class ChooseFolder extends Application {
 
 	@Override
-	public void start(Stage primaryStage) {
+	public void start(final Stage primaryStage) {
 
+		DialogResponse response = Dialogs.showConfirmDialog(primaryStage,
+				Message.UI_WELCOME_INFO_NO_FILE_FOUND, null,
+				Message.UI_WELCOME_TITLE);
+		
+		if(response.equals(response.YES)) {
+			openFileChooser(primaryStage);
+		} else {
+			System.exit(0);
+		}
+	}
+
+	protected void openFileChooser(Stage primaryStage) {
 		DirectoryChooser directoryChooser = new DirectoryChooser();
 		directoryChooser.setTitle("Choose Save Directory");
 		File selectedDirectory = directoryChooser.showDialog(primaryStage);
@@ -26,9 +49,9 @@ public class ChooseFolder extends Application {
 				String directoryPath = selectedDirectory.getCanonicalPath();
 				FileName.setCanonicalPath(directoryPath);
 				FileName.writeCanonicalToFile();
-				
+
 				FileName.createFileIfNotExist();
-				
+
 			} catch (IOException e) {
 				System.err.println(Message.FAIL_FILES_EXIST);
 				System.exit(1);
@@ -38,6 +61,5 @@ public class ChooseFolder extends Application {
 
 		}
 	}
-	
-	
+
 }
