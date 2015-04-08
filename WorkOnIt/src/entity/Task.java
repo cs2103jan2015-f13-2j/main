83e89943831cbc827e5a0272c9a3c90f8f4262df
taipan541 <taipan541@hotmail.com>
2015-04-08 21:01:36 +0800

    amend equals method for entities to exclude taskId

--- WorkOnIt/src/entity/Task.java
+++ WorkOnIt/src/entity/Task.java
@@ -81,10 +81,10 @@ public abstract class Task {
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
+		result = prime * result + (isCompleted ? 1231 : 1237);
 		result = prime * result + priority;
 		result = prime * result
 				+ ((sortDate == null) ? 0 : sortDate.hashCode());
-		result = prime * result + (int) (taskId ^ (taskId >>> 32));
 		result = prime * result
 				+ ((taskName == null) ? 0 : taskName.hashCode());
 		return result;
@@ -99,14 +99,14 @@ public abstract class Task {
 		if (getClass() != obj.getClass())
 			return false;
 		Task other = (Task) obj;
+		if (isCompleted != other.isCompleted)
+			return false;
 		if (priority != other.priority)
 			return false;
 		if (sortDate == null) {
 			if (other.sortDate != null)
 				return false;
 		} else if (!sortDate.equals(other.sortDate))
-			return false;
-		if (taskId != other.taskId)
 			return false;
 		if (taskName == null) {
 			if (other.taskName != null)
@@ -121,42 +121,36 @@ public abstract class Task {
 		@Override
 		public int compare(Task task1, Task task2) {
 			int returnVal;
-			
+
 			// sort task date in ascending order
 			Date task1Date = task1.getSortDate();
 			Date task2Date = task2.getSortDate();
-			
-			if(task1.isCompleted == task2.isCompleted)
-			{
-			if (task1Date.equals(task2Date)) {
-				// sort priority in descending order
-				// (high > medium > low)
-				int task1Priority = task1.getPriority();
-				int task2Priority = task2.getPriority();
-				if (task1Priority == task2Priority) {
-					// sort task by name
-					String task1Desc = task1.getTaskName();
-					String task2Desc = task2.getTaskName();
-					returnVal = task1Desc.compareTo(task2Desc);
-				} else {
-					returnVal = task2Priority - task1Priority;
-				}
+
+			if (task1.isCompleted == task2.isCompleted) {
+				if (task1Date.equals(task2Date)) {
+					// sort priority in descending order
+					// (high > medium > low)
+					int task1Priority = task1.getPriority();
+					int task2Priority = task2.getPriority();
+					if (task1Priority == task2Priority) {
+						// sort task by name
+						String task1Desc = task1.getTaskName();
+						String task2Desc = task2.getTaskName();
+						returnVal = task1Desc.compareTo(task2Desc);
+					} else {
+						returnVal = task2Priority - task1Priority;
+					}
 				} else {
 					returnVal = task1Date.compareTo(task2Date);
 				}
-			
-			}
-			else
-			{
-				if(task1.isCompleted)
-				{
+
+			} else {
+				if (task1.isCompleted) {
 					returnVal = 1;
-				}
-				else
-				{
+				} else {
 					returnVal = -1;
 				}
-				
+
 			}
 			return returnVal;
 		}
