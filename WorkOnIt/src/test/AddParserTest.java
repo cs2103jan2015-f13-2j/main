5d1cb969693c18a5010dfad12d9e478bbf746ca8
taipan541 <taipan541@hotmail.com>
2015-04-12 16:52:47 +0800

    include test case for retrieve parser

--- WorkOnIt/src/test/AddParserTest.java
+++ WorkOnIt/src/test/AddParserTest.java
@@ -2,16 +2,67 @@ package test;
 
 import static org.junit.Assert.*;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Map;
+
 import org.junit.Test;
 
 import data.InitFileIO;
 import entity.Success;
 import entity.Task;
 import parser.AddParser;
+import validator.Validator;
 
 public class AddParserTest {
 
 	InitFileIO initFile = null;
+
+	/**
+	 * This method test the addition of normal task correctly.
+	 */
+	// @author A0111916M
+	@Test
+	public void addNormalTask() {
+		initTestEnvironment();
+
+		String command = "this week from 12 April to 13 April";
+		String expected = "this week from 12 Apr 2015,  12:00:01 AM to 13 Apr 2015,  11:59:59 PM";
+
+		executeTestEquals(command, expected);
+	}
+
+	/**
+	 * This method test the addition of deadline task correctly.
+	 */
+	// @author A0111916M
+	@Test
+	public void addDeadlineTask() {
+		initTestEnvironment();
+
+		String command = "this week by 13 April";
+		String expected = "this week by 13 Apr 2015,  11:59:59 PM";
+
+		executeTestEquals(command, expected);
+	}
+
+	/**
+	 * This method test the addition of floating task correctly.
+	 */
+	// @author A0111916M
+	@Test
+	public void addFloatingTask() {
+		initTestEnvironment();
+
+		String command = "this week";
+		String expected = "this week";
+
+		executeTestEquals(command, expected);
+	}
+	
+	/**********************
+	 * NON-TEST METHODS
+	 *********************/
 
 	/**
 	 * This method initiates the required information before executing the test
@@ -35,50 +86,63 @@ public class AddParserTest {
 		Class[] argClasses = { String.class };
 		Object[] argObjects = { command };
 
-		Success status = PrivateMethodTester.invokePrivateMethod(
-				AddParser.class, "parseAddCommand", argClasses, argObjects);
+		Success status = invokePrivateMethod(AddParser.class,
+				"parseAddCommand", argClasses, argObjects);
 
 		Task task = (Task) status.getObj();
 		assertEquals(expected, task.toDisplay());
 	}
-
+	
 	/**
-	 * This method test the addition of normal task correctly.
+	 * This method initiates the required information before executing the test
+	 * cases.
+	 */
+	/**
+	 * @param targetClass
+	 *            the class of the method
+	 * @param methodName
+	 *            name of the method that want to be publicized
+	 * @param argClasses
+	 *            classes of the objects that is to be passed into the method
+	 * @param argObjects
+	 *            objects that is to be passed into the method
+	 * @return Success object
 	 */
 	// @author A0111916M
-	@Test
-	public void addNormalTask() {
-		initTestEnvironment();
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	public static Success invokePrivateMethod(Class targetClass,
+			String methodName, Class[] argClasses, Object[] argObjects) {
 
-		String command = "this week from today to tomorrow";
+		Success status = null;
 
-		executeTestEquals(command, command);
-	}
+		try {
+			Validator validator = new Validator();
+			Object t = targetClass.getDeclaredConstructor(Map.class)
+					.newInstance(validator.getKeywordFullMap());
+			Method method = targetClass.getDeclaredMethod(methodName,
+					argClasses);
+			System.out.println(method.getName());
+			method.setAccessible(true);
 
-	/**
-	 * This method test the addition of deadline task correctly.
-	 */
-	// @author A0111916M
-	@Test
-	public void addDeadlineTask() {
-		initTestEnvironment();
+			t = method.invoke(t, argObjects);
 
-		String command = "this week by tomorrow";
+			status = (Success) t;
 
-		executeTestEquals(command, command);
-	}
+		} catch (NoSuchMethodException e) {
+			System.err.println(e.getMessage());
+		} catch (SecurityException e) {
+			System.err.println(e.getMessage());
+		} catch (IllegalAccessException e) {
+			System.err.println(e.getMessage());
+		} catch (IllegalArgumentException e) {
+			System.err.println(e.getMessage());
+		} catch (InvocationTargetException e) {
+			System.err.println(e.getMessage());
+		} catch (InstantiationException e) {
+			System.err.println(e.getMessage());
+		}
 
-	/**
-	 * This method test the addition of floating task correctly.
-	 */
-	// @author A0111916M
-	@Test
-	public void addFloatingTask() {
-		initTestEnvironment();
-
-		String command = "this week";
-
-		executeTestEquals(command, command);
+		return status;
 	}
 
 }
--- WorkOnIt/src/test/RetrieveParserTest.java
+++ WorkOnIt/src/test/RetrieveParserTest.java
@@ -2,17 +2,72 @@ package test;
 
 import static org.junit.Assert.*;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Map;
+
 import org.junit.Test;
 
 import data.InitFileIO;
 import parser.AddParser;
+import parser.DataParser;
 import parser.RetrieveParser;
+import validator.Validator;
 import entity.Success;
 import entity.Task;
 
 public class RetrieveParserTest {
 
 	InitFileIO initFile = null;
+
+	/**
+	 * This method test if can retrieve a task with the following keyword
+	 * correctly.
+	 */
+	// @author A0111837J
+	@Test
+	public void retrieveKeyword() {
+
+		initTestEnvironment();
+
+		String command = "test";
+
+		executeTestTrue(command);
+	}
+
+	/**
+	 * This method test if can retrieve a task with a priority within a date
+	 * range.
+	 */
+	// @author A0111837J
+	@Test
+	public void retrieveKeywordPiorityDateRange() {
+
+		initTestEnvironment();
+
+		String command = "priority 2 from 10 April to 12 April";
+
+		executeTestTrue(command);
+	}
+
+	/**
+	 * This method test if can retrieve a task with a priority within a date
+	 * range.
+	 */
+	// @author A0111837J
+	@Test
+	public void retrieveKeywordDateRange() {
+
+		initTestEnvironment();
+
+		String command = "from 10 April to 12 April";
+
+		executeTestTrue(command);
+	}
+
+	/**********************
+	 * NON-TEST METHODS
+	 *********************/
 
 	/**
 	 * This method initiates the required information before executing the test
@@ -36,26 +91,63 @@ public class RetrieveParserTest {
 		Class[] argClasses = { String.class };
 		Object[] argObjects = { command };
 
-		Success status = PrivateMethodTester.invokePrivateMethod(
-				RetrieveParser.class, "parseRetrieveCommand", argClasses,
-				argObjects);
-
+		Success status = invokePrivateMethod(RetrieveParser.class,
+				"parseRetrieveCommand", argClasses, argObjects);
+		System.out.println(status == null);
 		assertTrue(status.isSuccess());
 	}
 
 	/**
-	 * This method test if can retrieve a task with the following keyword
-	 * correctly.
+	 * This method initiates the required information before executing the test
+	 * cases.
+	 */
+	/**
+	 * @param targetClass
+	 *            the class of the method
+	 * @param methodName
+	 *            name of the method that want to be publicized
+	 * @param argClasses
+	 *            classes of the objects that is to be passed into the method
+	 * @param argObjects
+	 *            objects that is to be passed into the method
+	 * @return Success object
 	 */
 	// @author A0111837J
-	@Test
-	public void retrieveKeyword() {
-		
-		initTestEnvironment();
-		
-		String command = "test";
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	public Success invokePrivateMethod(Class targetClass, String methodName,
+			Class[] argClasses, Object[] argObjects) {
 
-		executeTestTrue(command);
+		Success status = null;
+
+		try {
+			Validator validator = new Validator();
+			Object t = targetClass.getDeclaredConstructor(Map.class,
+					DataParser.class).newInstance(
+					validator.getKeywordFullMap(), new DataParser());
+			Method method = targetClass.getDeclaredMethod(methodName,
+					argClasses);
+			System.out.println(method.getName());
+			method.setAccessible(true);
+
+			t = method.invoke(t, argObjects);
+
+			status = (Success) t;
+
+		} catch (NoSuchMethodException e) {
+			System.err.println(e.getMessage());
+		} catch (SecurityException e) {
+			System.err.println(e.getMessage());
+		} catch (IllegalAccessException e) {
+			System.err.println(e.getMessage());
+		} catch (IllegalArgumentException e) {
+			System.err.println(e.getMessage());
+		} catch (InvocationTargetException e) {
+			System.err.println(e.getMessage());
+		} catch (InstantiationException e) {
+			System.err.println(e.getMessage());
+		}
+
+		return status;
 	}
 
 }
